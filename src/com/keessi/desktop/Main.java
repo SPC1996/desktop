58a6e6c643cd37f5b7a0fd849313ae3e497e5cf1
SPC1996 <1594475695@qq.com>
2017-08-24 15:05:26 +0800

    javafx 2 -- Login Form

--- src/com/keessi/desktop/Main.java
+++ src/com/keessi/desktop/Main.java
@@ -1,24 +1,67 @@
 package com.keessi.desktop;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Login Form");
 
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World"));
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        final Text actionTarget = new Text();
+        actionTarget.setFill(Color.FIREBRICK);
+        actionTarget.setFont(Font.font("Courier New", FontWeight.NORMAL, 12));
+        grid.add(actionTarget, 1, 7);
+
+        Text sceneTitle = new Text("Welcome to login Form");
+        sceneTitle.setFont(Font.font("Courier New", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label username = new Label("Username:");
+        grid.add(username, 0, 2);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 2);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 3);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 3);
+
+        Button loginBtn = new Button("    Sign in    ");
+        Button regBtn = new Button("    Register    ");
+        loginBtn.setOnAction(event -> actionTarget.setText("Sign in button pressed"));
+        regBtn.setOnAction(event -> actionTarget.setText("Register button pressed"));
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+        hBox.getChildren().add(loginBtn);
+        hBox.getChildren().add(regBtn);
+        grid.add(hBox, 1, 5);
+
+        Scene scene = new Scene(grid, 400, 275);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
